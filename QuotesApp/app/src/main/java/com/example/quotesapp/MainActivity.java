77ceda51751c64a64cfa8fb9e699dcb317bf61e3
ckpatel21 <chintanpatel2199@gmail.com>
2022-05-19 11:57:08 +0530

    Added one more app

---
+++ QuotesApp/app/src/main/java/com/example/quotesapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.quotesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView quote;
+    Button share;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        quote = findViewById(R.id.tvQuote);
+        share = findViewById(R.id.btnShare);
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sharetext = quote.getText().toString();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, sharetext);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
